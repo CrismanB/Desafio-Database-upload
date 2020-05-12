d3fcf3da1e0627972814ac2fee2781b0f96e7ff7
Crisman Bernardo Kima <crismanbernardokima@MacBook-Air-de-Crisman.local>
2020-05-12 16:47:03 -0300

    fix models

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -13,6 +14,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -22,8 +24,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
-
-  console.error(err);
 
   return response.status(500).json({
     status: 'error',
